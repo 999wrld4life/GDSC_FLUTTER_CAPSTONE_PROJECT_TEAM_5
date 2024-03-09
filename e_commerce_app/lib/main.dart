aaca8507aab1d22eb920c76da8e87998d5c7250b
Nathaniel <122879317+999wrld4life@users.noreply.github.com>
2024-03-09 11:12:42 +0300

    Added main.dart with all the necessary dependency injections and configurations

---
+++ e_commerce_app/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gdsc_captone_project/controllers/bottom_navigation_provider/bottom_navigation_provider.dart';
+import 'package:gdsc_captone_project/controllers/theme_provider/theme_provider.dart';
+import 'package:gdsc_captone_project/routes/routes.dart';
+import 'package:provider/provider.dart';
+import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => BottomNavigationProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: Provider.of<ThemeProvider>(context).themeData,
+        initialRoute: RouteClass.getAuthRoute(),
+        getPages: RouteClass.routes,
+      ),
+    );
+  }
+}
