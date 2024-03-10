da2ca9809384d0fbb37fdd8ab5989f54dec23f8b
Nathaniel <122879317+999wrld4life@users.noreply.github.com>
2024-03-10 10:25:58 +0300

    Updated button_with_icon.dart

--- e_commerce_app/lib/views/shared/buttons/button_with_icon.dart
+++ e_commerce_app/lib/views/shared/buttons/button_with_icon.dart
@@ -5,12 +5,14 @@ import 'package:gdsc_captone_project/views/shared/fonts/google_font.dart';
 class ButtonWithIcon extends StatelessWidget {
   final IconData? icon;
   final String text;
+  final double paddingLeft;
   final void Function()? onTap;
   const ButtonWithIcon({
     super.key,
     this.icon,
     required this.text,
     this.onTap,
+    required this.paddingLeft,
   });
 
   @override
@@ -25,7 +27,7 @@ class ButtonWithIcon extends StatelessWidget {
           borderRadius: BorderRadius.circular(25),
         ),
         child: Padding(
-          padding: EdgeInsets.only(left: 100.w),
+          padding: EdgeInsets.only(left: paddingLeft.w),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
